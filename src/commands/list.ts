065254579df9f4d3387a2636169ed9aa0fa9aceb
Kevin Daniel <kevindkevdan@gmail.com>
2026-07-08 17:23:47 -0400

    feat: impl command `wt-mgr list`

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,5 +1,7 @@
 // list all  git worktree sessions
 import { CommandModule } from "yargs";
+import { logger } from "../logger";
+import { readState } from "../state";
 // import { logger } from "../logger";
 // 
 export const command = 'list';
@@ -9,6 +11,25 @@ export const aliases = ['l'];
 
 
 export const handler: CommandModule<{}>['handler'] = async (argv) => {
-  console.log('hi')
+  try {
+    logger.info('Listing worktrees...\n');
+
+    // 1. Find the worktree in our state
+    const worktrees = readState()
+
+    // 2. List worktrees if any
+    if (worktrees.length == 0) {
+      logger.log("No worktrees are currently being managed.\n")
+      logger.info("Try `wt-mgr new <my-feature>` to create one.")
+      return;
+    } else {
+      logger.success('Managed  worktrees:')
+      console.table(worktrees)
+    }
+
+  } catch (error) {
+    const msg = error instanceof Error ? error.message : 'An unknown error occured.';
+    logger.error(`❌ Failed to list worktrees: ${msg}.`);
+  }
 };
 
